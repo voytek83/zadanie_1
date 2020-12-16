b2b1482d0d6c70f0abe93ec642f8a2d7894b5856
toczki <voytek83@gmail.com>
2020-12-16 19:28:31 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+	    String[] imiona = new String[5];
+
+	    for (int i=0;i<5;i++ ) {
+            System.out.println("Podaj swoje imię");
+            imiona[i] = scanner.next();
+        }
+
+	    for (int j=0;j<5;j++)
+            System.out.println("Witaj "+imiona[j]);
+
+    }
+}
