b5a8f134e601187f5caa5450af4c1e73f1a73520
Piotr Kaczanowski <pkaczanowski@agora.pl>
2020-12-18 11:27:13 +0100

    poprawki plik gitignore - sprawia ze do repo nie trafia wygenerowane klasy javy i pliki konfiguracyjne z IDE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,22 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> imiona = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {
+        String[] imiona = new String[5];
+
+        for (int i=0;i<5;i++ ) {
             System.out.println("Podaj swoje imię");
-            imiona.add(scanner.next());
+            imiona[i] = scanner.next();
         }
 
-        for (int j = 0; j < 5; j++) {
-            System.out.println("Witaj " + imiona.get(j));
+        for (int j=0;j<5;j++) {
+            System.out.println("Witaj " + imiona[j]);
         }
+
     }
 }
