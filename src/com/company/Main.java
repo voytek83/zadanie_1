068fa64085d8f84765d3466ec4a859d8783a8ada
Wojciech Wysocki <voytek83@gmail.com>
2021-01-09 16:03:58 +0100

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> imiona = new ArrayList<>();
 
-        String[] imiona = new String[5];
-
-        for (int i=0;i<5;i++ ) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Podaj swoje imię");
-            imiona[i] = scanner.next();
+            imiona.add(scanner.next());
         }
 
-        for (int j=0;j<5;j++) {
-            System.out.println("Witaj " + imiona[j]);
+        for (int j = 0; j < 5; j++) {
+            System.out.println("Witaj " + imiona.get(j));
         }
-
     }
 }
--- src/com/company/calc/Calc.java
+++ src/com/company/calc/Calc.java
@@ -11,34 +11,39 @@ public class Calc {
         System.out.println("Kalkulator");
         System.out.println("Podaj pierwszą liczbę:");
         double pierwsza = scanner.nextDouble();
-        System.out.println("wybierz działanie: 1.+ 2.- 3.* 4./ ");
-        int dzialanie = scanner.nextInt();
+        System.out.println("wpisz znak działania");
+        String dzialanie = scanner.next();
         System.out.println("Podaj drugą liczbę:");
-        double druga  = scanner.nextDouble();
+        double druga = scanner.nextDouble();
         System.out.print("Wynik:");
 
-        switch(dzialanie) {
-            case 1:
-                wynik = pierwsza + druga;
-                break;
-            case 2:
+        if (dzialanie.equals("+")) {
+            wynik = pierwsza + druga;
+        } else {
+            if (dzialanie.equals("-")) {
                 wynik = pierwsza - druga;
-                break;
-            case 3:
-                wynik = pierwsza * druga;
-                break;
-            case 4:
-                if (druga == 0)
-                    System.out.println("Nie można dzielić przez zero");
-                else
-                    wynik = pierwsza / druga;
-                break;
-            default:
-                System.out.println("nie wybrano poprawnie działania");
-                break;
+            } else {
+                if (dzialanie.equals("*")) {
+                    wynik = pierwsza * druga;
+                } else {
+                    if (dzialanie.equals("/")) {
+                        if (druga == 0) {
+                            System.out.println("Nie można dzielić przez zero");
+                        } else {
+                            wynik = pierwsza / druga;
+                        }
+
+                    } else {
+                        System.out.println(" nie może być pokazany - wprowadzono nieprawidłowy znak działania");
+                    }
+                }
+            }
+
         }
-                System.out.println(wynik);
+
+        System.out.println(wynik);
 
 
     }
 }
+
