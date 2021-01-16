b6d4bfc7b3ce5230ac1b5fd3697339edd07348d9
Wojciech Wysocki <voytek83@gmail.com>
2021-01-16 13:15:13 +0100

    update calc switch statement

--- src/com/company/calc/Calc.java
+++ src/com/company/calc/Calc.java
@@ -6,33 +6,19 @@ public class Calc {
 
     public static void main(String[] args) {
 
-//        double wynik = 0;
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Kalkulator");
         System.out.println("Podaj pierwszą liczbę:");
         double pierwsza = scanner.nextDouble();
+
         System.out.println("wpisz znak działania");
         String dzialanie = scanner.next();
+
         System.out.println("Podaj drugą liczbę:");
         double druga = scanner.nextDouble();
+
         System.out.print("Wynik:");
-
-
-/*        switch (dzialanie.toLowerCase(Locale.ROOT)) {
-            case "+":
-                wynik = Calculations.adding(pierwsza, druga);
-            case "-":
-                wynik = Calculations.subtract(pierwsza, druga);
-            case "*":
-                wynik = Calculations.multiply(pierwsza, druga);
-            case "/":
-                wynik = Calculations.divide(pierwsza, druga);
-                break;
-            default:
-                System.out.println(" nie może być pokazany - wprowadzono nieprawidłowy znak działania");
-        }
-*/
-        //wynik = Calculations.calculation(pierwsza,dzialanie,druga);
         System.out.println(Calculations.calculation(pierwsza, dzialanie, druga));
 
     }
--- src/com/company/calc/Calculations.java
+++ src/com/company/calc/Calculations.java
@@ -16,23 +16,3 @@ public class Calculations {
 }
 
 
-/*
-    static double adding(double a, double b) {
-        return a + b;
-    }
-
-    static double subtract(double a, double b) {
-        return a - b;
-
-    }
-
-    static double multiply(double a, double b) {
-        return a * b;
-
-    }
-
-    static double divide(double a, double b) {
-        return a / b;
-    }
-*/
-
